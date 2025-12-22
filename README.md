2d012d354d6036b13ffa80caab837869df37d809
Mohamed Nady <mohamed0011199@gmail.com>
2025-12-22 13:58:33 +0200

    adding filtering, priority, debuging the project totally

---
+++ README.md
@@ -0,0 +1,80 @@
+# go-todo-cli
+A to-do list tool for terminal, you can add tasks, remvoe, check as done/inprogress, filter list, sorting
+--
+## Installing
+from releases you can download latest version "go-todo-cli.exe"
+
+## Commands
+
+### add new task
+---
+`add` command adds task content (head) and priority that optional with default value of routine
+
+```bash
+add priority <priority value> <task>
+```
+priority value can be one of the next:
+- urgent
+- important
+- routine (default value)
+- luxury
+---
+if your task priority value is routine so the command could be like follwing
+```bash
+add <task>
+```
+
+### show to-do list
+---
+`show` command presents your list in a table of columns ID, task, status (to-do, inprogress, or done), priority, age (time sence adding the task to the list), createdat (date and time for task adding), completedat (when task be chekced as done this value adding with checking date and time)
+
+```bash
+show
+```
+result could be like following table
+| ID | TITLE | STATUS | PRIORITY | AGE | CREATED AT | COMPLTED AT |
+|----| ----- | ------ | -------- | --- | ---------- | ----------- |
+| 1 | do math homework | to-do | urgent | 2 hours ago | 2025-12-21 21:15:23 | |
+| 2 | collect 10000 trophy in clash royale | in-progress | important | 20 days ago | 2025-12-01 23:35:46 | |
+
+### remove task
+---
+`remove` command removes task from the list by its ID (using show command to get IDs, it's auto increment)
+
+```bash
+remove <ID>
+```
+
+### start doing task
+---
+`start` command changes status from to-do to in-progress by its ID
+```bash
+start <ID>
+```
+
+### check task as done
+---
+`check` command changes status from to-do or in-progress to done by its ID
+```bash
+check <ID>
+```
+
+### change priority
+---
+`priority` command changes priority between (urgent, important, routine, luxury)by its ID
+```bash
+priority <ID> <new priority value>
+```
+### clear list
+---
+`clear` command remove all tasks from the list and be totally empty
+```bash
+clear
+```
+
+### help
+---
+`help` command print helping message
+```bash
+help
+```
--- main.go
+++ main.go
@@ -1,14 +1,5 @@
 package main
 
-/*
-simple to-do list
-fearutes to be added later
-multi list
-sort based on: creation time, how much urgent "taken add input", alphapeticly (ascending and decending)
-all data saved to json
-commads are add,start (change status to in progress), check (as done), remove, show, clear (the list), create (later for multilists), sort type_of_sort (default by creation time) order (default ascending)
-maybe ui later if needed
-*/
 import (
 	"bufio"
 	"encoding/json"
@@ -17,20 +8,23 @@ import (
 	"strconv"
 	"strings"
 	"time"
+
+	"github.com/jedib0t/go-pretty/v6/table"
+	"github.com/jedib0t/go-pretty/v6/text"
 )
 
 type task struct {
 	ID          int       `json:"id"`
 	Title       string    `json:"title"`
 	Description string    `json:"description"`
-	Status      string    `json:"Status"`
+	Status      string    `json:"status"`
+	Priority    string    `json:"priority"`
 	Createdat   time.Time `json:"createdat"`
 	Completedat time.Time `json:"completedat"`
 }
 
-var todos []task
-
 func loadTasks() ([]task, error) {
+	var tasks []task
 	file, err := os.ReadFile("tasks.json")
 	if err != nil {
 		if os.IsNotExist(err) {
@@ -38,9 +32,8 @@ func loadTasks() ([]task, error) {
 		}
 		return nil, err
 	}
-	err = json.Unmarshal(file, &todos)
-	return todos, err
-
+	err = json.Unmarshal(file, &tasks)
+	return tasks, err
 }
 
 func saveTasks(tasks []task) error {
@@ -50,21 +43,45 @@ func saveTasks(tasks []task) error {
 	}
 	return os.WriteFile("tasks.json", data, 0644)
 }
-func addTask(title string) error {
+func addTask(title string, priority string) error {
 	tasks, err := loadTasks()
 	if err != nil {
 		return err
 	}
+	maxID := 0
+	for i := range tasks {
+		if tasks[i].ID > maxID {
+			maxID = tasks[i].ID
+		}
+	}
 	toadd := task{
-		ID:        len(tasks) + 1,
+		ID:        maxID + 1,
 		Title:     title,
 		Status:    "to-do",
+		Priority:  priority,
 		Createdat: time.Now(),
 	}
 	tasks = append(tasks, toadd)
 	return saveTasks(tasks)
 }
-func showTasks() error {
+func timeAgo(tim time.Time) string {
+	age := time.Since(tim)
+
+	switch {
+	case age < time.Minute:
+		return fmt.Sprintf("%d seconds ago", int(age.Seconds()))
+
+	case age < time.Hour:
+		return fmt.Sprintf("%d minutes ago", int(age.Minutes()))
+
+	case age < 24*time.Hour:
+		return fmt.Sprintf("%d hours ago", int(age.Hours()))
+
+	default:
+		return fmt.Sprintf("%d days ago", int(age.Hours()/24))
+	}
+}
+func showTasks(filter string) error {
 	tasks, err := loadTasks()
 	if err != nil {
 		return err
@@ -73,7 +90,53 @@ func showTasks() error {
 		fmt.Println("No tasks found")
 		return nil
 	}
-
+	tabl := table.NewWriter()
+	tabl.SetOutputMirror(os.Stdout)
+	tabl.AppendHeader(table.Row{
+		"ID", "Title", "Status", "Priority", "Age", "Created At", "Completed At",
+	})
+	for i := range tasks {
+		if filter != "" {
+			if tasks[i].Status != filter && tasks[i].Priority != filter {
+				continue
+			}
+		}
+		status := tasks[i].Status
+		switch status {
+		case "to-do":
+			status = text.FgRed.Sprint("TO-DO")
+		case "in-progress":
+			status = text.FgYellow.Sprint("IN-PROGRESS")
+		case "done":
+			status = text.FgGreen.Sprint("DONE")
+		}
+		priority := tasks[i].Priority
+		switch priority {
+		case "routine":
+			priority = text.FgGreen.Sprint("Routine")
+		case "urgent":
+			priority = text.FgRed.Sprint("Urgent")
+		case "important":
+			priority = text.FgYellow.Sprint("important")
+		case "luxury":
+			priority = text.FgBlue.Sprint("Luxury")
+		default:
+			priority = text.FgHiWhite.Sprint(priority)
+		}
+		title := tasks[i].Title
+		if tasks[i].Status == "done" {
+			title = text.FgHiBlack.Sprint(tasks[i].Title)
+		}
+		completedat := " "
+		if !tasks[i].Completedat.IsZero() {
+			completedat = tasks[i].Completedat.Format("2006-01-02 15:04:05")
+		}
+		age := timeAgo(tasks[i].Createdat)
+		tabl.AppendRow(table.Row{tasks[i].ID, title, status, priority, age, tasks[i].Createdat.Format("2006-01-02 15:04:05"), completedat})
+	}
+	tabl.SetStyle(table.StyleDouble)
+	tabl.Render()
+	return nil
 }
 func inprogress(taskID int) error {
 	tasks, err := loadTasks()
@@ -109,6 +172,20 @@ func checkTask(taskid int) error {
 	return fmt.Errorf("task not found, ID may be wrong")
 
 }
+
+func changePriority(taskID int, newpriorrity string) error {
+	tasks, err := loadTasks()
+	if err != nil {
+		return err
+	}
+	for i := range tasks {
+		if tasks[i].ID == taskID {
+			tasks[i].Priority = newpriorrity
+			return saveTasks(tasks)
+		}
+	}
+	return fmt.Errorf("task not found, ID may be wrong")
+}
 func removeTask(taskID int) error {
 	tasks, err := loadTasks()
 	if err != nil {
@@ -125,6 +202,7 @@ func removeTask(taskID int) error {
 func clearList() error {
 	return saveTasks([]task{})
 }
+
 func main() {
 
 	var current_command string
@@ -136,41 +214,124 @@ func main() {
 		}
 		splited := strings.Fields(current_command)
 		switch splited[0] {
+
 		case "add":
+			if len(splited) < 2 {
+				fmt.Println("missing task content")
+				continue
+			}
+			if splited[1] == "priority" {
+				if splited[2] == "urgent" || splited[2] == "important" || splited[2] == "luxury" {
+					title := strings.Join(splited[3:], " ")
+					err := addTask(title, splited[2])
+					if err != nil {
+						fmt.Println("Error in adding your item: ", err)
+						continue
+					}
+					fmt.Println(title, " added to your to-do list succesfully")
+					continue
+				}
+			}
 			title := current_command[3:]
-			addTask(title)
-			fmt.Println(title, " add to your to-do list succesfully")
+			priority := "routine"
+			err := addTask(title, priority)
+			if err != nil {
+				fmt.Println("Error in adding your item: ", err)
+				continue
+			}
+			fmt.Println(title, " added to your to-do list succesfully")
+
 		case "show":
-			tasks, err := loadTasks()
-			fmt.Println(tasks, err)
+			filter := ""
+			if len(splited) < 1 {
+				filter = splited[1]
+			}
+			err := showTasks(filter)
+			if err != nil {
+				fmt.Println("error: ", err)
+				continue
+			}
+
 		case "start":
+			if len(splited) < 2 {
+				fmt.Println("missing task ID")
+				continue
+			}
 			number_of_task, err := strconv.Atoi(splited[1])
 			if err != nil {
 				fmt.Printf("error in extracting task ID from your command %v\n", err)
-				return
+				continue
 			}
-			inprogress(number_of_task)
+			err1 := inprogress(number_of_task)
+			if err1 != nil {
+				fmt.Println("error: ", err1)
+				continue
+			}
 			fmt.Println("task no.", number_of_task, " in progress now, go finish the job")
+
 		case "check":
+			if len(splited) < 2 {
+				fmt.Println("missing task ID")
+				continue
+			}
 			number_of_task, err := strconv.Atoi(splited[1])
 			if err != nil {
 				fmt.Printf("error in extracting task ID from your command %v\n", err)
-				return
+				continue
 			}
-			checkTask(number_of_task)
+			err1 := checkTask(number_of_task)
+			if err1 != nil {
+				fmt.Println("error: ", err1)
+				continue
+			}
 			fmt.Println("task no.", number_of_task, " checked as done succesfully")
+
+		case "priority":
+			if len(splited) < 3 {
+				fmt.Println("missing task ID or new priority")
+				continue
+			}
+
+			number_off_task, err := strconv.Atoi(splited[1])
+			if err != nil {
+				fmt.Printf("error in extracting task ID from your command %v\n", err)
+				continue
+			}
+
+			err1 := changePriority(number_off_task, splited[2])
+			if err1 != nil {
+				fmt.Println("error: ", err1)
+				continue
+			}
+			fmt.Println("Priority of task no.", number_off_task, " has changed to ", splited[2])
+
 		case "remove":
+			if len(splited) < 2 {
+				fmt.Println("missing task ID")
+				continue
+			}
 			number_of_task, err := strconv.Atoi(splited[1])
 			if err != nil {
 				fmt.Printf("error in extracting task ID from your command %v\n", err)
+				continue
 			}
-			removeTask(number_of_task)
+			err1 := removeTask(number_of_task)
+			if err1 != nil {
+				fmt.Println("error: ", err1)
+				continue
+			}
 			fmt.Println("task no.", number_of_task, " removed succesfully from your list")
+
 		case "clear":
-			clearList()
+			err := clearList()
+			if err != nil {
+				fmt.Println("error: ", err)
+				continue
+			}
 			fmt.Println("list cleared succesfully")
+
 		case "help":
-			fmt.Println("This a to-do lists app where you can add tasks to do\n\nCommands\n\"add <task>\" to add new task as following\n\n\"show\" show table of all tasks at your list\n\n\"start <task ID>\" start doing the task, status be in porgress\n\n\"check <task ID>\" check the task as done\n\n\"remove <task ID>\" remove task from the list\n\n\"clear\" clear all data at the table\n\n\"help\" print this help message")
+			fmt.Println("This a to-do lists app where you can add tasks to do\n\nCommands\n\"add <task>\" to add new task as following \noptional set priority: \"add priority <priority level> <task>\" where prioty levels in order are:\nurgent\nimportant\nroutine\nluxury\nnote: the default is routine level where you don't have to set priority manually\n\n\"show\" show table of all tasks at your list\noptional filter by priority or status \"show <filter>\"\n\n\"start <task ID>\" start doing the task, status be in porgress\n\n\"check <task ID>\" check the task as done\n\n\"priority <task ID> <new value>\" change default priority that routine to new one from (urgent ,important, luxury)\n\n\"remove <task ID>\" remove task from the list\n\n\"clear\" clear all data at the table\n\n\"help\" print this help message")
 		}
 	}
 	if scanner.Err() != nil {
